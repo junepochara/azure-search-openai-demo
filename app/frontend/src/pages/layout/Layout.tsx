a245722f9d48fac53a85aa17beaa4b41f5f87898
Pocharatham (June) Sinlapanuntakul <157505436+junepochara@users.noreply.github.com>
2024-02-05 22:03:06 +0000

    Customize headlines

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -14,7 +14,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>Employee Handbook Chat App</h3>
+                        <h3 className={styles.headerTitle}>Harmonia Employee Handbook Chat App</h3>
                     </Link>
                     <nav>
                         <ul className={styles.headerNavList}>
